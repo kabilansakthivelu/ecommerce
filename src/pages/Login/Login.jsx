39376d6ed895d8853d4b743400126b400900a42f
Kabilan Sakthivelu <88023922+kabilansakthivelu@users.noreply.github.com>
2024-03-22 01:27:59 +0000

    Login page Styling changes

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,19 +6,19 @@ const Login = () => {
     <div className="container">
       <h3 className="container--heading">Login</h3>
       <p className="container--sub-heading">Welcome back to ECOMMERCE</p>
-      <p className="container-message">The next gen business marketplace</p>
+      <p className="container--message">The next gen business marketplace</p>
       <form>
-        <div>
+        <div className="container--formField">
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+        <input type="email" id="email" placeholder="Enter" className="container--formField__input"/>
         </div>
-      <div>
+      <div className="container--formField">
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input type="password" id="password" placeholder="Enter" className="container--formField__input"/>
         </div>
-        <button>Login</button>
+        <button className="container--button">LOGIN</button>
         <hr />
-        <p>Don't have an account? <Link to="/register">SIGN UP</Link></p>
+        <p className="container--navigation">Don't have an account? <Link to="/register">SIGN UP</Link></p>
       </form>
     </div>
     );
